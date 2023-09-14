7ee65ee64d55fd94b897628b206241da35355b09
bpe <bpe@voxteneo.com>
2023-09-14 08:31:56 +0200

    Fixing favicons and manifest

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,6 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import { Inter } from 'next/font/google'
 import Header from '@/app/_components/shared/header'
 import Footer from '@/app/_components/shared/footer'
-import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <Head>
+    <head>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
@@ -23,7 +22,7 @@ export default function RootLayout({ children }) {
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#020917"/>
         <meta name="msapplication-TileColor" content="#020917"/>
         <meta name="theme-color" content="#020917"/>
-    </Head>
+    </head>
 
       <body className={`${inter.className} bg-main text-main-text`}>
         <Header/>
