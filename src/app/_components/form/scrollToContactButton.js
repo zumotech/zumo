686ee839b3d7501a624cecde7633b9d81ed9f477
bpe <bpe@voxteneo.com>
2023-09-17 16:29:38 +0200

    Updating content

--- src/app/_components/form/scrollToContactButton.js
+++ src/app/_components/form/scrollToContactButton.js
@@ -10,11 +10,11 @@ const ScrollToContactButton = () => {
 
   return (
     <>
-    {/* eslint-disable-next-line react/no-unescaped-entities */}
     <button
       type="button"
       onClick={handleScrollToContact}
       className="outline-none py-4 px-5 text-xl font-bold text-main text-center rounded bg-red-500 hover:bg-red-600 sm:w-fit bg-gradient-to-br from-yellow-300 to-red-500 hover:from-red-500 hover:to-yellow-300">
+      {/* eslint-disable-next-line react/no-unescaped-entities */}
       Let's work together</button>
     </>
 
